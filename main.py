77e47c9e6b4dbfe6a56880a58e63ca4c19054a7f
aldi <agoy587@gmail.com>
2021-10-09 22:46:16 +0700

    karate

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ from mss import mss
 
 keyboard.wait("s")
 
-branch_left = cv2.imread("branch_left.png")
-branc_right = cv2.imread("branch_right.png")
+branch_left = cv2.cvtColor(cv2.imread("branch_left2.png"), cv2.COLOR_BGR2GRAY)
+branc_right = cv2.cvtColor(cv2.imread("branch_right2.png"), cv2.COLOR_BGR2GRAY)
+
+
 branch_img = branch_left
 
 SCT = mss()
-left_dimensions = {'left': 1300, 'top': 630, 'width': 180, 'height': 150}
-right_dimensions = {'left': 1650, 'top': 630, 'width': 180, 'height': 150}
+left_dimensions = {'left': 1400, 'top': 690, 'width': 180, 'height': 150}
+right_dimensions = {'left': 1550, 'top': 690, 'width': 180, 'height': 150}
 isLeft = True
 xx = 1500
 
-
-# pyautogui.click(1650, 600)
 fps_time = time()
 while True:
     if(isLeft):
@@ -29,16 +29,16 @@ while True:
         img = np.array(SCT.grab(right_dimensions))
         branch_img = branc_right
 
-    scr_remove = img[:, :, :3]
+    scr_remove = cv2.cvtColor(img[:, :, :3], cv2.COLOR_BGR2GRAY)
 
     result = cv2.matchTemplate(scr_remove, branch_img, cv2.TM_CCOEFF_NORMED)
 
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
 
-    w = branch_img.shape[1]
-    h = branch_img.shape[0]
-    threshold = .60
+    # w = branch_img.shape[1]
+    # h = branch_img.shape[0]
+    threshold = .50
 
     if max_val > threshold:
         print(f"Max Val : ", max_val)
@@ -48,16 +48,16 @@ while True:
         else :
             isLeft = True
             xx = 1500
-        cv2.rectangle(img, max_loc, (max_loc[0] + w, max_loc[1] + h), (0, 255, 0), 2)
+        print(isLeft)
+        # cv2.rectangle(img, max_loc, (max_loc[0] + w, max_loc[1] + h), (0, 255, 0), 2)
 
-    cv2.imshow('Screen Shot', img)
-    cv2.waitKey(1)
-    # print(xx)
+    # cv2.imshow('Screen Shot', img)
+    # cv2.waitKey(1)
     pyautogui.click(xx, 600)
-    sleep(.09)
+    sleep(.15)
     #close window/
     if keyboard.is_pressed('q'):
-        # cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
         start = False
         break
 
